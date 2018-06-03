e8d9c3deef6c3a2081d58380cc2e3d1ff6c4c663
Evgeny Chuvpilo xt84 <echuvpilo@gmail.com>
2018-06-03 15:05:11 +0300

    Initial commit

---
+++ src/main/scala/xt84/info/decisionmapper/test/Main.scala
@@ -0,0 +1,30 @@
+package xt84.info.decisionmapper.test
+
+object Main extends App {
+  val KEYS = Map(
+    "data"    -> "Path to data",
+    "rules"   -> "Path to transformation rules configuration",
+    "report"  -> "Path to report file"
+  )
+
+  override def main(args: Array[String]): Unit = {
+    if ((args.length == 0) || ((args.length % KEYS.size) > 0) || (args.length < 3)) {
+      println(usage())
+    } else {
+      //TODO: job init code
+    }
+  }
+
+  def usage(): String = KEYS.map(kv => kv._1 -> s"--${kv._1} ${kv._2}").values.mkString(" ")
+
+  def parseArguments(argsMap: Map[String, String] = Map(), args: List[String]): Map[String, String] = {
+    def isKey(arg: String) = arg.startsWith("--")
+    args match {
+      case Nil => argsMap
+      case arg :: args if isKey(arg) => parseArguments(
+        argsMap ++ Map(arg.replaceAll("--", "") -> args.head),
+        args.tail
+      )
+    }
+  }
+}
